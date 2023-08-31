cc4331ed1530301c715f84e835a33929b0db9406
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-08-31 05:19:38 +0200

    Actualizar info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,13 +1,5 @@
-const handler = async (m, {conn, usedPrefix}) => {
-  const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-  const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `http://buitre.boxmineworld.com'
-    'footer': wm,
-    // 'buttons':[
-    // {buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1},
-    // {buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
+isplayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
     'headerType': 6};
   conn.sendMessage(m.chat, buttonMessage, {quoted: m});
 };
-handler.command = ['instalarbot', 'instalarbot'];
-export default handler;
+hand
