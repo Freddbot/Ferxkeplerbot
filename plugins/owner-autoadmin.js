c366c4c6c5b6254bbc955a6f2b3c3a5b0e97728b
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-09-08 04:55:08 +0200

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,5 +1,3 @@
-/* Creditos a https://github.com/unptoadrih15/UPABOT-MD */
-
 const handler = async (m, {conn, isAdmin}) => {
   if (m.fromMe) return;
   if (isAdmin) throw '*[❗] 𝙷𝙾𝙻𝙰 𝙲𝚁𝙴𝙰𝙳𝙾𝚁, 𝙲𝙾𝙼𝙾 𝙴𝚂𝚃𝙰? 𝚄𝚂𝚃𝙴𝙳 𝚈𝙰 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*';
