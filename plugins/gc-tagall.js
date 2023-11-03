88338840820026262b6dda7eb74a127c0f645228
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-11-03 15:00:54 +0100

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,9 +6,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝗔𝗖𝗧𝗜𝗩𝗘𝗡𝗦𝗘 𝗕𝗜𝗡𝗔𝗥𝗜@𝗦⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀🕸️🎃𝗔𝗖𝗧𝗜𝗩𝗘𝗡𝗦𝗘 𝗕𝗜𝗡𝗔𝗥𝗜@𝗦🏚️🕷️⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `🌬️ @${mem.id.split('@')[0]}\n`;
+    teks += `👻 @${mem.id.split('@')[0]}\n`;
   }
   teks += `*└* By @iambuitre\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
