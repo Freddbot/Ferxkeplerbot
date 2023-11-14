9fa8819d2c7eee201da875c63aa12c745c01c95f
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-11-14 14:54:56 +0100

    Actualizar fun-8vs8.js

--- plugins/fun-8vs8.js
+++ plugins/fun-8vs8.js
@@ -5,29 +5,29 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`🥷🏽┊ *_BUITRE LIST_*┊🥷🏽
+m.reply(`🧝🏼‍♂️┊ *_BUITRE LIST_*┊🧝🏼‍♂️
 
 *MODO:*  𝟖 𝐕𝐒 𝟖
 
 𝔼𝕊ℂ𝕌𝔸𝔻ℝ𝔸 #𝟙 
 
-🥷🏽⇉ 
+🎅🏼⇉ 
 
-🥷🏽⇉ 
+🎅🏼⇉ 
 
-🥷🏽⇉ 
+🎅🏼⇉ 
 
-🥷🏽⇉  
+🎅🏼⇉  
 
 𝔼𝕊ℂ𝕌𝔸𝔻ℝ𝔸 #𝟚
 
-🥷🏽⇉ 
+🎅🏼⇉ 
 
-🥷🏽⇉
+🎅🏼⇉
 
-🥷🏽⇉
+🎅🏼⇉
 
-🥷🏽⇉ 
+🎅🏼⇉ 
 
 *_LISTA BY @iambuitre_*`, null, {
 mentions: [a, b]
